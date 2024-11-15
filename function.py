52d66054b9f97646876488242691e0136763fd12
JiarongGuo <1950955951@qq.com>
2024-11-15 23:28:11 +0800

    finish function write

--- function.py
+++ function.py
@@ -1,7 +1,9 @@
 def write():
     try:
-        file=open(r'points','r')
+        file = open(r'points', 'r')
     except FileNotFoundError:
         return {}
     else:
-        pass
+        ret = eval(file.read())
+        return ret
+
